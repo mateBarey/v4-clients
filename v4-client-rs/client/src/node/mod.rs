7ecb79191054806195328c0c8d50cb8cc75d7d2e
George Cubas <georgeraulc@gmail.com>
2025-05-06 14:03:04 -0500

    Update mod.rs

--- v4-client-rs/client/src/node/mod.rs
+++ v4-client-rs/client/src/node/mod.rs
@@ -3,16 +3,22 @@ mod client;
 mod config;
 mod fee;
 pub mod order;
-/// Account number sequencing mechanisms
 pub mod sequencer;
 mod types;
 mod utils;
 mod wallet;
 
+// Bring in the proto definitions from the local submodule (forked version)
+pub mod dydx_proto;
+
+// Explicit re-exports to simplify usage downstream
 pub use builder::TxBuilder;
 pub use client::{error::*, Address, Authenticator, NodeClient, Subaccount, TxHash};
 pub use config::NodeConfig;
 pub use order::*;
+pub use sequencer::*;
 pub use types::ChainId;
 pub use utils::BigIntExt;
 pub use wallet::{Account, PublicAccount, Wallet};
+
+pub use dydx_proto::dydxprotocol; // So you can use `dydx::dydxprotocol::clob::Order` consistently
